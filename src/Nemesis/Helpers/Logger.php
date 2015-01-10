6fbafc2e8211fc4b965a8b9b517a2ab6b348b547
Roger Spielmann <puncher@braveheart.ch>
2015-01-10 15:14:22 +0100

    logger class added

---
+++ src/Nemesis/Helpers/Logger.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Puncher
+ * Date: 10/01/15
+ * Time: 15:07
+ */
+
+namespace Nemesis\Helpers;
+
+
+class Logger {
+
+    private $level = Array(
+        'info' => '<info>',
+        'comment' => '<comment>',
+        'question' => '<question>',
+        'error' => '<error>'
+    );
+
+}
